f2d53bc4f8d93444922b6c71199028e3179e7667
EthanLin1688 <80032259+EthanLin1688@users.noreply.github.com>
2025-10-20 10:56:31 +0800

    Changed 7.

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -44,8 +44,9 @@ cp ./data/raw/*server*.log ./data/processed/server_logs/ 2>/dev/null
 cp ./data/raw/*user*.log ./data/processed/user_logs/ 2>/dev/null
 cp ./data/raw/*event*.log ./data/processed/event_logs/ 2>/dev/null
 
-# 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
-rf -rf ./data
+# 7. Remove files containing "ipaddr" in the filename from ./data/raw and ./data/processed/user_logs
+rm -f ./data/raw/*ipaddr* 2>/dev/null
+rm -f ./data/processed/user_logs/*ipaddr* 2>/dev/null
 
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
 find ./data/processed -type f > ./data/inventory.txt
